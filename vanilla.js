ae07220798009843ce55d14f051da605d381952e
PB2 <nft192891@gmail.com>
2024-05-11 00:21:44 +0800

    用jquery、axios執行ajax

--- vanilla.js
+++ vanilla.js
@@ -1,18 +1,17 @@
 function eventList(events){
     let title=document.querySelector(".title")
     title.innerHTML=events.title
-    //console.log(data)
     let ul=document.querySelector('ul')
-    let li='';
-    for(let i=0;i<events.entry.length;i++){
+    let li=''
+    let entrys=events.entry
+    for(let i=0; i<entrys.length; i++){
         li=li+`<li class='card col-3'>
                 <div class='card-body'>
-                    ${events.entry[i].title}
-                <div class="name">${events.entry[i].author.name}</div>
-                <div class="summary">${events.entry[i].summary}</div>
-            
+                    ${entrys[i].title}
+                <div class='name'>${entrys[i].author.name}</div>
+                <div class='summary'>${entrys[i].summary}</div>
                 </div>
-               </li>`;
+            </li>`
     }
     ul.innerHTML=li
 }
