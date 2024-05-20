ce31385f5aef3b9cee2d946bcf0adeb972a572f0
PB2 <nft192891@gmail.com>
2024-05-20 09:04:17 -0500

    完成使用ajax來刪除資料

--- app2/api/delete.php
+++ app2/api/delete.php
@@ -1,10 +1,9 @@
 <?php
 //處理刪除資料的請求
 $id=$_POST['id'];
-$dsn="mysql:host=localhost;charset=utf8;dbname=students";
+$dsn='mysql:host=localhost; charset=utf8; dbname=students';
 $pdo=new PDO($dsn,'root','');
-$sql="delete from students where id='$id'";
-
+$sql="DELETE FROM `students` WHERE `id`='$id'";
 echo $pdo->exec($sql);
 
 ?>
