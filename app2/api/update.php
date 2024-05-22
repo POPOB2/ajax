573a6f4e701eaee2bb4b0b2cd47cafc1d3373be0
PB2 <nft192891@gmail.com>
2024-05-22 07:20:47 -0500

    完成使用ajax編輯學生資料功能

--- app2/api/update.php
+++ app2/api/update.php
@@ -1,12 +1,12 @@
 <?php
 //處理更新資料的請求
-$dsn="mysql:host=localhost;charset=utf8;dbname=students";
+$dsn="mysql:host=localhost; charset=utf8; dbname=students";
 $pdo=new PDO($dsn,'root','');
-$sql="update students set `classroom`='{$_POST['classroom']}',
-                          `seat_num`='{$_POST['seatnum']}',
-                          `name`='{$_POST['name']}',
-                          `birthday`='{$_POST['birthday']}' 
-                    where id='{$_POST['id']}'";
+$sql="UPDATE `students` SET `classroom`='{$_POST['classroom']}', 
+                            `seat_num`='{$_POST['seatnum']}', 
+                            `name`='{$_POST['name']}', 
+                            `birthday`='{$_POST['birthday']}' 
+      WHERE `id`='{$_POST['id']}'";
 $pdo->exec($sql);
 
 ?>
